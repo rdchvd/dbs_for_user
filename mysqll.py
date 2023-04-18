f76a313481ed172770dddb7376360c7319cb842e
darynarodicheva <daryna.r@gowombat.team>
2023-04-18 21:57:10 +0300

    refactor(lab-1): fix linter issues

--- mysqll.py
+++ mysqll.py
@@ -1,4 +1,3 @@
-
 import psycopg2
 import psycopg2.extras
 from create import create
@@ -9,16 +8,17 @@ from update import update
 from mysqll_connection import *
 from ifexist import table_check
 
+
 def input_tb():
-    tb=input('Введіть назву таблиці: ')
+    tb = input('Введіть назву таблиці: ')
     return tb
 
+
 def new_tb(tb):
-    answer=input('Продовжити роботу з таблицею %s? так/ні: '%tb)
+    answer = input('Продовжити роботу з таблицею %s? так/ні: ' % tb)
     if answer == 'ні':
         tb = input_tb()
     return tb
-
 
 
 def input_format(st):
@@ -39,26 +39,14 @@ def tb_list(cur, conn, tb):
     print(tblist)
 
 
-
-
-
-
-
-
-
-
-
-
 def main():
-    
-    menu = {'вставити':insert,'вивести':read,'відредагувати':update,'видалити':delete}
+    menu = {'вставити': insert, 'вивести': read, 'відредагувати': update, 'видалити': delete}
     tb = None
     conn = mysql_connect()
     cur = conn.cursor()
-    
-    
+
     while True:
-        
+
         tb = input_tb() if tb == None else new_tb(tb)
         all_tbs = table_check(cur, conn, tb)
         if tb not in all_tbs:
@@ -71,19 +59,18 @@ def main():
 
             print("Виберіть операцію: ")
             for key in menu.keys():
-                print("-> "+key)
+                print("-> " + key)
             ans = input(">>")
             menu[ans](cur, conn, tb)
             if ans == 'видалити':
                 tb = None
         ans = input("Продовжити роботу?так/ні: ")
-            
+
         if ans == 'ні':
             break
 
     conn.close()
     return
 
-main()
 
- 
+main()
